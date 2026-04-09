766862b3325c11ea3bc9821262520555f54adfec
고은호 <ttaseu100810@gmail.com>
2026-04-09 12:41:05 +0900

    2주차 과제

---
+++ week2/practice/script.js
@@ -0,0 +1,6 @@
+const cheerButton = document.getElementById('cheerButton');
+
+cheerButton.addEventListener('click', () => {
+  cheerButton.textContent = '고은호의 계획 제출하기!';
+  alert('웹스터디 첫 과제 제출 성공!');
+});
